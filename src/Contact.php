104145f7da4a106d75306ec8b6f10cd9a8af1bde
Brian Kropff <brian.kropff@gmail.com>
2015-03-06 10:45:01 -0800

    Assessment start

---
+++ src/Contact.php
@@ -0,0 +1,29 @@
+<?php
+  class Contact
+  {
+    private $name;
+    private $phone;
+    private $address;
+
+    function __construct(){
+
+
+      get___
+
+
+      set__
+
+
+
+    }
+
+
+
+  }
+
+
+
+
+return $app;
+
+?>
