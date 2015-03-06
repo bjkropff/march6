45fb6395d19b3736ba0694bd08b7317a6502e74a
brian.kropff@gmail.com <student@epicodus.com>
2015-03-06 13:29:50 -0800

    working homepage

--- app/app.php
+++ app/app.php
@@ -2,6 +2,36 @@
   require_once __DIR__."/../vendor/autoload.php";
   require_once __DIR__."/../src/Contact.php";
 
+  session_start();
+
+  if (empty ($_SESSION['list_of_contacts'])) {
+      $_SESSION['list_of_contacts'] = array();
+
+  }
+
   $app = new Silex\Application();
+
+  $app->register(new Silex\Provider\TwigServiceProvider(), array(
+      'twig.path' => __DIR__."/../views"
+  ));
+
+  $app->get("/", function() use ($app) {
+
+      return $app['twig']->render('contacts.twig', array('contact' => Contact::getAll()));
+  });
+
+  $app->post("/contacts", function() use ($app) {
+      $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+      $contact->save();
+      return $app['twig']->render('add.twig', array('newContact' => $contact));
+
+  });
+
+  $app->post("/delete", function() use ($app) {
+      Contact::deleteAll();
+      return $app['twig']->render('delete.twig');
+  });
+
+  return $app;
 
 ?>
--- src/Contact.php
+++ src/Contact.php
@@ -1,29 +1,60 @@
 <?php
-  class Contact
-  {
+ class Contact
+ {
     private $name;
     private $phone;
     private $address;
 
-    function __construct(){
+    function __construct($name, $phone, $address){
 
-
-      get___
-
-
-      set__
-
-
-
+        $this->name = $name;
+        $this->phone = $phone;
+        $this->address = $address;
     }
 
+      function setName($new_name)
+      {
+          $this->name = (string) $new_name;
+      }
 
+      function getName()
+      {
+          return $this->name;
+      }
 
-  }
+      function setPhone($new_phone)
+      {
+          $this->name = (string) $new_phone;
+      }
 
+      function getPhone()
+      {
+          return $this->phone;
+      }
 
+      function setAddress($new_address)
+      {
+          $this->address = (string) $new_address;
+      }
 
+      function getAddress()
+      {
+          return $this->address;
+      }
 
-return $app;
+      function save()
+      {
+          array_push($_SESSION['list_of_contacts'], $this);
+      }
 
+      static function getAll()
+      {
+          return $_SESSION['list_of_contacts'];
+      }
+
+      static function deleteAll()
+      {
+          $_SESSION['list_of_contacts'] = array();
+      }
+}
 ?>
